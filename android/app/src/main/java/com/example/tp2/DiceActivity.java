e4679a86d90b6e8c6dea6681b6517a5db4704806
Med Aziz Akaichi <akaichiaziz10@gmail.com>
2026-07-15 19:54:53 +0100

    tp 2CREEZ UNE APPLICATION AVEC UNE

---
+++ android/app/src/main/java/com/example/tp2/DiceActivity.java
@@ -0,0 +1,39 @@
+package com.example.tp2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.security.SecureRandom;
+
+public class DiceActivity extends Activity implements View.OnClickListener {
+
+    private TextView textResult;
+    private int max;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dice);
+
+        max = getIntent().getIntExtra("max", 0);
+
+        TextView textTitle = (TextView) findViewById(R.id.textTitle);
+        textTitle.setText(max + " sided dice");
+
+        textResult = (TextView) findViewById(R.id.textResult);
+        textResult.setText("");
+
+        Button buttonRoll = (Button) findViewById(R.id.buttonRoll);
+        buttonRoll.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        SecureRandom random = new SecureRandom();
+        int result = random.nextInt(max) + 1;
+        textResult.setText(String.valueOf(result));
+    }
+}
